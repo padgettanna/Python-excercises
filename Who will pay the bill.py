dc71568356689be0ac44da1c5de475bdc94dfa00
Anna Padgett <119985556+padgettanna@users.noreply.github.com>
2022-12-12 19:42:13 -0800

    Update Who will pay the bill.py

--- Who will pay the bill.py
+++ Who will pay the bill.py
@@ -5,5 +5,9 @@ names = names_string.split(", ")
 
 number_of_names = len(names)
 random_number = random.randint(0, number_of_names - 1)
+person_pays = names[random_number]
 
-print(names[random_number] + " is going to buy meal today!")
+# or use line 11 instead of lines 6, 7 and 8
+#person_pays = random.choice(names)
+
+print(person_pays + " is going to buy meal today!")
