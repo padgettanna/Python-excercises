4af599f26a7479bff8ab7ea139aa5f9bbb6b3086
Anna Padgett <119985556+padgettanna@users.noreply.github.com>
2022-12-14 09:27:35 -0800

    Create Rock, Paper, Scissors.py

---
+++ Rock, Paper, Scissors.py
@@ -0,0 +1,65 @@
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+
+choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+if choice == 0:
+  print(rock)
+elif choice == 1:
+  print(paper)
+elif choice == 2:
+  print(scissors)
+else:
+  print("Error")
+
+computers_choise = random.choice([rock, paper, scissors])
+print("Computer chose:\n" + computers_choise)
+
+if choice == 0:
+  if computers_choise == paper:
+    print("You lose.")
+  elif computers_choise == scissors:
+    print("You win!")
+  else:
+    print("It's a draw.")
+
+elif choice == 1:
+  if computers_choise == scissors:
+    print("You lose.")
+  elif computers_choise == rock:
+    print("You win!")
+  else:
+    print("It's a draw.")
+
+else:
+  if computers_choise == rock:
+    print("You lose.")
+  elif computers_choise == paper:
+    print("You win!")
+  else:
+    print("It's a draw.")
